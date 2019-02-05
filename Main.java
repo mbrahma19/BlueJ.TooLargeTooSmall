9824ce657db468a0db123666d08e088cdd2256a1
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-8.local>
2019-02-05 18:17:21 -0500

    Completed TooLargeTooSmall game. Functions are in the main function

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
-
+import java.util.Scanner;
+import java.util.Random;
 /**
  * Write a description of class Main here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Marshilla Brahma)
+ * @version (2/5/2019)
  */
 public class Main
 {
@@ -18,16 +19,43 @@ public class Main
         // initialise instance variables
         x = 0;
     }
+    
+   
+    public static void main(String[] args ){
+        boolean correct = false;
+        int counter = 1;
+        
+        System.out.println("Try to guess the Mystery number! The range is 0 - 10");
+        
+        Scanner userInput = new Scanner(System.in);
+        int username = userInput.nextInt();
+        
+        Random rand = new Random();
+        int x = rand.nextInt(10);
+        
+        while(correct == false){
+            if(username != x){
+                if(username>x){
+                    System.out.println("Too high, guess again");
+                    username = userInput.nextInt();
+                    counter++;
+                    System.out.println("Number of guesses: " + counter);
+                }
+                else{
+                    System.out.println("Too low, guess again");
+                    username = userInput.nextInt();
+                    counter++;
+                    System.out.println("Number of guesses: " + counter);
+                }
+            }
+            else{
+                System.out.println("You got the correct answer! It was " + x);
+                System.out.println("Number of guesses: " + counter);
+                correct = true;
+            }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        }  
+
+
     }
 }
